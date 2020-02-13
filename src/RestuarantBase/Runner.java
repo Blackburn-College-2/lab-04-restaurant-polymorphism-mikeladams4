d7597b26df4511a443c8f72297f2605957af3ebf
mikeladams4 <adamsmik4@gmail.com>
2020-02-13 11:02:01 -0600

    inlab, not done

--- src/RestuarantBase/Runner.java
+++ src/RestuarantBase/Runner.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package emptyjavaproject;
+package RestuarantBase;
 
 /**
  *
  * @author paul.kline
  */
-public class Main {
+public class Runner {
 
     /**
      * @param args the command line arguments
